04d588bb40d17fa41de978786646b23b0ed3170b
major_capital <46876914+kopchanni@users.noreply.github.com>
2022-11-15 21:06:34 +0500

    headers security

---
+++ headers_checks.py
@@ -0,0 +1,25 @@
+import requests
+# https://github.com/wkeeling/selenium-wire
+domain = 'https://rebiz.com'
+
+
+def check_headers(domain: str):
+    re = requests.get(url='https://'+domain).headers
+    # print(re)
+
+
+    header_checks = ['X-Frame-Options', 'Strict-Transport_Security', 'X-Content-Type-Options',
+                     'Content-Security-Policy', 'X-Frame-Options', 'X-XSS-Protection',
+                     'Referrer-Policy', 'Feature-Policy']
+
+    for head in header_checks:
+        if head in re:
+            with open('headers_output.txt', 'w') as file:
+                file.write(domain + 'No threats')
+            # print(domain + 'No threats')
+        elif head not in re:
+            with open('headers_output.txt', 'w') as file:
+                file.write(domain + " Header Threat(s): " + head)
+            # print('------------------------------------')
+            # print(domain + " Header Threat(s): " + head)
+
